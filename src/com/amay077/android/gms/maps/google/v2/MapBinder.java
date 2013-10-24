80e8006a96841355905079e0003de784ec47945c
Hironobu OKUYAMA <okuokuoku@gmail.com>
2013-10-25 01:39:28 +0900

    WIP: Implement toMarkers

--- src/com/amay077/android/gms/maps/google/v2/MapBinder.java
+++ src/com/amay077/android/gms/maps/google/v2/MapBinder.java
@@ -4,17 +4,25 @@ import hu.akarnokd.reactive4java.base.Action1;
 import hu.akarnokd.reactive4java.base.Action2;
 import hu.akarnokd.reactive4java.base.Func0;
 import hu.akarnokd.reactive4java.base.Func1;
+import hu.akarnokd.reactive4java.base.Func2;
 import hu.akarnokd.reactive4java.base.Option;
 
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import jp.co.cosmoroot.android.gms.maps.IconDescriptor;
+import jp.co.cosmoroot.android.gms.maps.MarkerSchema;
 import jp.co.cosmoroot.android.gms.maps.model.LatLon;
 import jp.co.cosmoroot.android.gms.maps.model.LatLonBounds;
 
+import com.amay077.android.collections.Lambda;
 import com.amay077.android.mvvm.BaseBinder;
 import com.amay077.android.mvvm.Binder;
 import com.amay077.android.mvvm.Binder.BindingType;
+import com.amay077.android.mvvm.SelectCommand;
 import com.amay077.android.types.Animate;
 import com.amay077.android.types.Padding;
 import com.amay077.lang.IProperty;
@@ -22,16 +30,20 @@ import com.amay077.lang.ObservableValue;
 import com.amay077.lang.Event.EventHandler;
 import com.amay077.lang.IProperty.OnValueChangedListener;
 import com.amay077.lang.IProperty._OnValueChangedListener;
+import com.amay077.lang.Predicate1;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import android.graphics.Bitmap;
 import android.location.Location;
+import android.util.Pair;
 import android.view.View;
 
 public class MapBinder extends BaseBinder {
@@ -213,6 +225,189 @@ public class MapBinder extends BaseBinder {
 		});
 	}
 	
+	public <T, S> void toMarkers(final IProperty<T> p, final Func1<T, Iterable<MarkerSchema>> converter,
+			final SelectCommand<S> command, final Func1<String, Option<S>> inverser) {
+
+		final Map<String, Pair<Marker, MarkerSchema>> markers = new HashMap<String, Pair<Marker, MarkerSchema>>();
+		
+		onChangedOnUiThread(p, new OnValueChangedListener<T>() {
+
+			@Override
+			public void onChanged(T newValue, T oldValue) {
+				for (Pair<Marker, MarkerSchema> p : markers.values()) {
+					p.first.remove();
+				}
+				markers.clear();
+
+				if (converter == null) {
+					return;
+				}
+				
+				Iterable<MarkerSchema> schemas = converter.invoke(newValue);
+				Lambda.iter(schemas, new Action1<MarkerSchema>() {
+					@Override
+					public void invoke(MarkerSchema s) {
+						if (s == null) {
+							return;
+						}
+						
+						Option<MarkerOptions> o = toMarkerOptions(s);
+						if (Option.isNone(o)) {
+							return;
+						}
+						
+						Marker marker = _map.get().addMarker(o.value());
+						markers.put(marker.getId(), new Pair<Marker, MarkerSchema>(marker, s));
+					}
+
+				});
+			}
+		});
+		
+		_map.markerClicked.add(new EventHandler<Marker>() {
+			@Override
+			public void handle(Object sender, Marker data) {
+				
+				if (converter == null) {
+					return;
+				}
+
+				Pair<Marker, MarkerSchema> pair = markers.get(data.getId());
+				if (pair == null || inverser == null) {
+					return;
+				}
+				
+				Option<S> item = inverser.invoke(pair.second.getId());
+				if (item != null && Option.isSome(item)) {
+					executeSelectCommand(command, item.value());
+				}
+			}
+		});
+	}
+	
+	public <T, S, U> void toMarkers2(final IProperty<T> p1, final IProperty<U> p2, final Func2<T, U, Iterable<MarkerSchema>> converter,
+			final SelectCommand<S> command, final Func1<String, Option<S>> inverser) {
+
+		final Map<String, Pair<Marker, MarkerSchema>> markers = new HashMap<String, Pair<Marker, MarkerSchema>>();
+		
+		final Action2<T, U> handler = new Action2<T, U>() {
+
+			@Override
+			public void invoke(T newValue, U newValue2) {
+				for (Pair<Marker, MarkerSchema> p : markers.values()) {
+					p.first.remove();
+				}
+				markers.clear();
+
+				if (converter == null) {
+					return;
+				}
+				
+				Iterable<MarkerSchema> schemas = converter.invoke(newValue, newValue2);
+				Lambda.iter(schemas, new Action1<MarkerSchema>() {
+					@Override
+					public void invoke(MarkerSchema s) {
+						if (s == null) {
+							return;
+						}
+						
+						Option<MarkerOptions> o = toMarkerOptions(s);
+						if (Option.isNone(o)) {
+							return;
+						}
+						
+						Marker marker = _map.get().addMarker(o.value());
+						markers.put(marker.getId(), new Pair<Marker, MarkerSchema>(marker, s));
+					}
+
+				});
+			}
+		};
+		
+		onChangedOnUiThread(p1, new OnValueChangedListener<T>() {
+			@Override
+			public void onChanged(T newValue, T oldValue) {
+				handler.invoke(newValue, p2.get());
+			}
+		});
+		onChangedOnUiThread(p2, new OnValueChangedListener<U>() {
+			@Override
+			public void onChanged(U newValue, U oldValue) {
+				handler.invoke(p1.get(), newValue);
+			}
+		});
+		
+		_map.markerClicked.add(new EventHandler<Marker>() {
+			@Override
+			public void handle(Object sender, Marker data) {
+				
+				if (converter == null) {
+					return;
+				}
+
+				Pair<Marker, MarkerSchema> pair = markers.get(data.getId());
+				if (pair == null || inverser == null) {
+					return;
+				}
+				
+				Option<S> item = inverser.invoke(pair.second.getId());
+				if (item != null && Option.isSome(item)) {
+					executeSelectCommand(command, item.value());
+				}
+			}
+		});
+	}
+
+	
+	private Option<MarkerOptions> toMarkerOptions(MarkerSchema schema) {
+		
+		LatLon pos = schema.getPosition();
+		if (pos == null) {
+			return Option.none();
+		}
+		
+		MarkerOptions options = new MarkerOptions();
+		LatLng latLng = toLatLng(schema.getPosition());
+		options = options.position(latLng).draggable(false);
+		
+		IconDescriptor icon = schema.getIcon();
+		if (icon != null) {
+			BitmapDescriptor descriptor = toBitmapDescriptor(icon);
+			if (descriptor != null) {
+				options = options.icon(descriptor);
+			}
+		}
+		
+		String title = schema.getTitle();
+		if (title != null) {
+			options = options.title(title);
+		}
+
+		String snippet = schema.getSnippet();
+		if (snippet != null) {
+			options = options.snippet(snippet);
+		}
+		
+		options = options.anchor(schema.getAnchorU(), schema.getAnchorU());
+		
+		return Option.some(options);
+	}
+
+	private BitmapDescriptor toBitmapDescriptor(IconDescriptor icon) {
+		switch (icon.getType()) {
+		case Default:
+			return BitmapDescriptorFactory.defaultMarker(icon.getHue());
+		case Asset:
+			return BitmapDescriptorFactory.fromAsset(icon.getAsset().value());
+		case Bitmap:
+			return BitmapDescriptorFactory.fromBitmap(icon.getBitmap());
+		case Resource:
+			return BitmapDescriptorFactory.fromResource(icon.getResource().value());
+		default:
+			return BitmapDescriptorFactory.defaultMarker();
+		}
+	}
+
 	public void toBounds(IProperty<Option<LatLonBounds>> dispBounds, final Padding padding, final Animate animate) {
 		toBounds(dispBounds, padding, animate, new Func1<Option<LatLonBounds>, Option<LatLonBounds>>() {
 			@Override
